8557c413e45c7a209720b2326b9729a249d64c38
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-09-05 20:54:25 -0300

    instale morgan

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
@@ -7,4 +9,11 @@ app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), ()=>{
     console.log(`Mi backend está en el puerto ${app.get('port')}`);
 })
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('./public'));
 
